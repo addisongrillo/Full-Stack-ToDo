e528a923ca4cc3b6dc56a03e80c4a926240c9cc8
Laz <lquin008@gmail.com>
2019-05-22 12:12:28 -0400

    remove new edit update and show actions as well as api code

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :tasks
+  root 'tasks#index'
+  resources :tasks, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
