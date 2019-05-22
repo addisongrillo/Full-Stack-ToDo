d5ab09fb169cd3faca6b61b46fb2e27a7b9cd520
Andy Weiss <andyweiss1982@gmail.com>
2019-05-22 15:07:13 -0400

    bugfix: default false

--- db/migrate/20190522183047_add_completed_to_tasks.rb
+++ db/migrate/20190522183047_add_completed_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddCompletedToTasks < ActiveRecord::Migration[6.0]
   def change
-    add_column :tasks, :completed, :boolean, null: false
+    add_column :tasks, :completed, :boolean, null: false, default: false
   end
 end
