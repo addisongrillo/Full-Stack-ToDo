d4efb821a1656c75df9d57817daa92a513d61c15
Laz <lquin008@gmail.com>
2019-05-24 12:04:31 -0400

    implement due dates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_184555) do
+ActiveRecord::Schema.define(version: 2019_05_24_153210) do
 
   create_table "sub_tasks", force: :cascade do |t|
     t.string "description"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_05_23_184555) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "completed", default: false, null: false
+    t.date "due_date"
   end
 
   add_foreign_key "sub_tasks", "tasks"
--- spec/system/user_views_tasks_spec.rb
+++ spec/system/user_views_tasks_spec.rb
@@ -29,4 +29,31 @@ RSpec.describe "viewing tasks", type: :system, js: true do
       expect(page).to have_text "Completed Task"
     end
   end
+  context "when a task is past due" do
+    it "displays the due_date in red" do
+      task = Task.create(description: "past due", due_date: 1.day.ago.to_date)
+      visit root_path
+ 
+      color = find("#task_#{task.id} .due_date").native.css_value('background-color')
+      expect(color).to eq('rgba(220, 53, 69, 1)')
+    end
+  end
+  context "when a task is due soon" do
+    it "displays the due_date in yellow" do
+      task = Task.create(description: "due soon", due_date: Date.tomorrow)
+      visit root_path
+ 
+      color = find("#task_#{task.id} .due_date").native.css_value('background-color')
+      expect(color).to eq('rgba(255, 193, 7, 1)')
+    end
+  end
+  context "when a task is due later" do
+    it "displays the due_date in green" do
+      task = Task.create(description: "due later", due_date: 10.days.from_now.to_date)
+      visit root_path
+ 
+      color = find("#task_#{task.id} .due_date").native.css_value('background-color')
+      expect(color).to eq('rgba(40, 167, 69, 1)')
+    end
+  end
 end
