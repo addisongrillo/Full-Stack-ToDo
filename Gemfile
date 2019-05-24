5e05950a499f194dae0abbe4336d1b951be07a91
Andy Weiss <andyweiss1982@gmail.com>
2019-05-24 15:11:09 -0400

    add pagination"

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '>= 2.5'
 
+gem 'will_paginate-bootstrap4'
 gem 'faker'
 gem 'hirb'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
---
+++ spec/system/user_paginates_tasks_spec.rb
@@ -0,0 +1,67 @@
+require "rails_helper"
+ 
+RSpec.describe "paginating tasks", type: :system, js: true do
+  context "when the user visits the root_path" do
+    it "displays the first five tasks" do
+      (1..10).each{|n| Task.create(description: "Task #{n}") }
+      visit root_path
+ 
+      expect(page).to have_text("Task 1")
+      expect(page).to have_text("Task 2")
+      expect(page).to have_text("Task 3")
+      expect(page).to have_text("Task 4")
+      expect(page).to have_text("Task 5")
+      expect(page).to_not have_text("Task 6")
+    end
+  end
+  context "when the user requests the second page" do
+    it "displays the next five tasks" do
+      (1..10).each{|n| Task.create(description: "Task #{n}") }
+      visit root_path
+      first('*[rel="next"]').click
+ 
+      expect(page).to have_text("Task 6")
+      expect(page).to have_text("Task 7")
+      expect(page).to have_text("Task 8")
+      expect(page).to have_text("Task 9")
+      expect(page).to have_text("Task 10")
+      expect(page).to_not have_text("Task 5")
+    end
+    context "when the user requests completed tasks" do
+      it "displays the second page of completed tasks" do
+        (1..10).each{|n| Task.create(description: "Completed Task #{n}", completed: true) }
+        (1..10).each{|n| Task.create(description: "Pending Task #{n}", completed: false) }
+ 
+        visit root_path(completed: true)
+        click_link("2")
+ 
+        expect(page).to have_text "Completed Task 6"
+        expect(page).to have_text "Completed Task 7"
+        expect(page).to have_text "Completed Task 8"
+        expect(page).to have_text "Completed Task 9"
+        expect(page).to have_text "Completed Task 10"
+        expect(page).to_not have_text "Pending Task"
+        expect(page).to_not have_text "Completed Task 5"
+      end
+      context "and the user requests past_due tasks" do
+        it "displays the second page of completed past_due tasks" do
+          (1..10).each{|n| Task.create(description: "Past Due Completed Task #{n}", completed: true, due_date: Date.yesterday) }
+          (1..10).each{|n| Task.create(description: "Due Soon Completed Task #{n}", completed: true, due_date: Date.tomorrow) }
+          (1..10).each{|n| Task.create(description: "Pending Task #{n}", completed: false) }
+ 
+          visit root_path
+          find('#menu-past-due').click
+          click_link("2")
+ 
+          expect(page).to have_text("Past Due Completed Task 6")
+          expect(page).to have_text("Past Due Completed Task 7")
+          expect(page).to have_text("Past Due Completed Task 8")
+          expect(page).to have_text("Past Due Completed Task 9")
+          expect(page).to have_text("Past Due Completed Task 10")
+          expect(page).to_not have_text("Due Soon Completed Task")
+          expect(page).to_not have_text("Pending Task")
+        end
+      end
+    end
+  end
+end
