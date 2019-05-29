7b06715fca5a12727574990faaab66312da68cc0
Addison Grillo <addisongrillo@gmail.com>
2019-05-29 10:49:46 -0400

    Display Tasks Component on home page

---
+++ app/javascript/bundles/Tasks/components/Tasks.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+ 
+class Tasks extends Component {
+  render(){
+    return(
+      <h1>I am the Tasks Component!</h1>
+    )
+  }
+}
+
+export default Tasks
---
+++ app/javascript/packs/app-bundle.js
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails'
+
+import Tasks from '../bundles/Tasks/components/Tasks'
+
+// This is how react_on_rails can see the Tasks in the browser.
+ReactOnRails.register({
+  Tasks,
+})
