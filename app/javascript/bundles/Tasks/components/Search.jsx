56dbb73ebbc12c7fce5501f93a200bfe8f5ef8b7
Andy Weiss <andyweiss1982@gmail.com>
2019-05-29 14:32:51 -0400

    id

--- app/javascript/bundles/Tasks/components/Search.jsx
+++ app/javascript/bundles/Tasks/components/Search.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Search = props => (
   <div className="form-group">
     <input
+      id="search"
       type="search"
       className={ props.invalid ? "form-control is-invalid" : "form-control" }
       placeholder="Search"
